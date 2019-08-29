9aac10a6698908d9d8def4677d69065108f99744
yui-har <232538+yui-har@users.noreply.github.com>
2019-08-29 17:56:48 +0900

    Remove unnecessary

--- init.rb
+++ init.rb
@@ -1,8 +1,6 @@
 require 'redmine'
 require 'redmine_s3'
 
-require_dependency 'redmine_s3_hooks'
-
 Redmine::Plugin.register :redmine_s3 do
   name 'S3'
   author 'Chris Dell'
--- lib/redmine_s3.rb
+++ lib/redmine_s3.rb
@@ -1,11 +1,9 @@
 require 'redmine_s3/attachment_patch'
 require 'redmine_s3/attachments_controller_patch'
-require 'redmine_s3/application_helper_patch'
 require 'redmine_s3/thumbnail_patch'
 require 'redmine_s3/connection'
 
 Redmine::Thumbnail.__send__(:include, RedmineS3::ThumbnailPatch)
 Attachment.__send__(:include, RedmineS3::AttachmentPatch)
-ApplicationHelper.__send__(:include, RedmineS3::ApplicationHelperPatch)
 AttachmentsController.__send__(:include, RedmineS3::AttachmentsControllerPatch)
 RedmineS3::Connection.create_bucket
