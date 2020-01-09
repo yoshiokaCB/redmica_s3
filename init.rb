038f0fbff34e23ee22aada3ee24e05d7836fa902
yui-har <232538+yui-har@users.noreply.github.com>
2020-01-09 11:14:32 +0900

    Requires Redmine 4.1.0 or higher

--- init.rb
+++ init.rb
@@ -2,6 +2,7 @@ require 'redmine'
 require 'redmine_s3' unless Rails.env.test?
 
 Redmine::Plugin.register :redmine_s3 do
+  requires_redmine version_or_higher: '4.1.0'
   name 'S3'
   author 'Chris Dell'
   description 'Use Amazon S3 as a storage engine for attachments'
