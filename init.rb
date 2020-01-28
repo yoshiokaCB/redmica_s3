a90e8342923c8294d050edf4faee9569261283c1
yui-har <232538+yui-har@users.noreply.github.com>
2020-01-28 10:55:02 +0900

    Move the procedure of lib/redmine_s3.rb to init.rb

--- init.rb
+++ init.rb
@@ -1,4 +1,7 @@
-require 'redmine_s3'
+require 'redmine_s3/attachment_patch'
+require 'redmine_s3/attachments_controller_patch'
+require 'redmine_s3/thumbnail_patch'
+require 'redmine_s3/connection'
 
 Redmine::Plugin.register :redmica_s3 do
   requires_redmine version_or_higher: '4.1.0'
@@ -6,4 +9,11 @@ Redmine::Plugin.register :redmica_s3 do
   author 'Far End Technologies, Inc.'
   description 'Use Amazon S3 as a storage engine for attachments'
   version '0.0.3'
+
+  Rails.configuration.to_prepare do
+    Redmine::Thumbnail.__send__(:include, RedmineS3::ThumbnailPatch)
+    Attachment.__send__(:include, RedmineS3::AttachmentPatch)
+    AttachmentsController.__send__(:include, RedmineS3::AttachmentsControllerPatch)
+  end
+  RedmineS3::Connection.create_bucket
 end
