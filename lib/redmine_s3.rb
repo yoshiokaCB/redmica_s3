9f5b2acc494da9928e4cdda004f6fd9946bd7e97
yui-har <232538+yui-har@users.noreply.github.com>
2020-01-28 10:58:55 +0900

    Delete lib/redmine_s3.rb

--- lib/redmine_s3.rb
+++
@@ -1,9 +0,0 @@
-require 'redmine_s3/attachment_patch'
-require 'redmine_s3/attachments_controller_patch'
-require 'redmine_s3/thumbnail_patch'
-require 'redmine_s3/connection'
-
-Redmine::Thumbnail.__send__(:include, RedmineS3::ThumbnailPatch)
-Attachment.__send__(:include, RedmineS3::AttachmentPatch)
-AttachmentsController.__send__(:include, RedmineS3::AttachmentsControllerPatch)
-RedmineS3::Connection.create_bucket
