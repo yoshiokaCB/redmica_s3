ba74f50fcbd1a9df3580686fbb89f475d1a277f6
yui-har <232538+yui-har@users.noreply.github.com>
2020-01-28 10:08:57 +0900

    Change plugin-name

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 require 'redmine_s3'
 
-Redmine::Plugin.register :redmine_s3 do
+Redmine::Plugin.register :redmica_s3 do
   requires_redmine version_or_higher: '4.1.0'
-  name 'S3'
+  name 'RedMica S3 plugin'
   author 'Far End Technologies, Inc.'
   description 'Use Amazon S3 as a storage engine for attachments'
   version '0.0.3'
