503dddae403ee902414dda4d2b7583bae45b7b8a
yui-har <232538+yui-har@users.noreply.github.com>
2020-01-28 10:05:14 +0900

    Change author-name

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require 'redmine_s3'
 Redmine::Plugin.register :redmine_s3 do
   requires_redmine version_or_higher: '4.1.0'
   name 'S3'
-  author 'Chris Dell'
+  author 'Far End Technologies, Inc.'
   description 'Use Amazon S3 as a storage engine for attachments'
   version '0.0.3'
 end
